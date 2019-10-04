ca8f27a3e3f7cf721a9fb75e263ce5ae4ed7d7ef
rkopec91 <ryankopec91@gmail.com>
2019-10-04 01:08:34 -0400

    Finished model.py and added model.h5 (This might not work do to problems testing)

--- model.py
+++ model.py
@@ -1,10 +1,12 @@
 import os
 import sys
+from keras import backend as K
 from keras.models import Sequential
 from keras.layers import Flatten, Dense, Lambda, Dropout, Conv2D, Cropping2D
 import csv
 import cv2
 import numpy as np
+from tensorflow.python.client import device_lib
 
 def read_data(path):
     lines = []
@@ -16,13 +18,28 @@ def read_data(path):
             lines.append(line)
 
     for line in lines:
-        source_path = line[0]
-        filename = source_path.split("/")[-1]
-        current_path = os.path.join(path,'IMG/' + filename)
-        image = cv2.imread(current_path)
-        images.append(image)
-        measurement = float(line[3])
-        measurements.append(measurement)
+        for i in range(3):
+            source_path = line[i]
+
+            filename = source_path.split("/")[-1]
+
+            current_path = os.path.join(path,'IMG/' + filename)
+
+            image = cv2.imread(current_path)
+
+            images.append(image)
+            images.append(np.fliplr(image))
+
+            measurement = float(line[3])
+            if i == 1:
+                measurements.append(measurement+0.2)
+                measurements.append(-measurement-0.2)
+            elif i == 2:
+                measurements.append(measurement-0.2)
+                measurements.append(-measurement+0.2)
+            else:
+                measurements.append(measurement)
+                measurements.append(-measurement)
 
     return lines, np.array(images), np.array(measurements)
 
@@ -42,27 +59,21 @@ def create_model():
     model.add(Dense(50))
     model.add(Dense(10))
     model.add(Dense(1))
+    model.compile(optimizer='adam', loss='mse')
     return model
 
-def train_model(model, x_train, y_train):
-    history_object = model.fit_generator(train_generator, samples_per_epoch= \
-                 len(train_samples), validation_data=validation_generator, \
-                 nb_val_samples=len(validation_samples), nb_epoch=3, verbose=1)
-
-    print(history_object.history.keys())
-    print('Loss')
-    print(history_object.history['loss'])
-    print('Validation Loss')
-    print(history_object.history['val_loss'])
+def train_model(model, X_train, y_train):
+    
+    model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=20)
 
     # Save model
     model.save('model.h5')
 
 if __name__ == "__main__":
-    
+    print(device_lib.list_local_devices())
+    K.tensorflow_backend._get_available_gpus()
+
     lines, X_train, y_train = read_data("./data/")
     model = create_model()
 
-    model.compile(optimizer='adam', loss='mse')
-    
     train_model(model, X_train, y_train)
