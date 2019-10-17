f4d449d5f642e839cc6df6ea280f4ab6546a6f76
rkopec91 <ryankopec91@gmail.com>
2019-10-17 01:31:00 -0400

    Add generator.

--- model.py
+++ model.py
@@ -1,47 +1,64 @@
 import os
 import sys
-from keras import backend as K
+import keras
 from keras.models import Sequential
 from keras.layers import Flatten, Dense, Lambda, Dropout, Conv2D, Cropping2D
 import csv
 import cv2
 import numpy as np
 from tensorflow.python.client import device_lib
+import tensorflow as tf
+import sklearn
+from sklearn.utils import shuffle
+from sklearn.model_selection import train_test_split
 
-def read_data(path):
-    lines = []
-    images = []
-    measurements = []
-    with open(os.path.join(path,'driving_log.csv')) as csv_file:
-        reader = csv.reader(csv_file)
+
+def read_lines():
+    samples = []
+    with open('./data/driving_log.csv') as csvfile:
+        reader = csv.reader(csvfile)
         for line in reader:
-            lines.append(line)
+            samples.append(line)
+    return samples
 
-    for line in lines:
-        for i in range(3):
-            source_path = line[i]
 
-            filename = source_path.split("/")[-1]
+def generator(samples, batch_size=32):
+    num_samples = len(samples)
+    while 1: # Loop forever so the generator never terminates
+        shuffle(samples)
+        for offset in range(0, num_samples, batch_size):
+            batch_samples = samples[offset:offset+batch_size]
 
-            current_path = os.path.join(path,'IMG/' + filename)
+            images = []
+            angles = []
+            for batch_sample in batch_samples:
+                for i in range(3):
+                    source_path = batch_sample[i]
 
-            image = cv2.imread(current_path)
+                    filename = source_path.split("/")[-1]
 
-            images.append(image)
-            images.append(np.fliplr(image))
+                    current_path = os.path.join('./data/IMG/', filename)
 
-            measurement = float(line[3])
-            if i == 1:
-                measurements.append(measurement+0.2)
-                measurements.append(-measurement-0.2)
-            elif i == 2:
-                measurements.append(measurement-0.2)
-                measurements.append(-measurement+0.2)
-            else:
-                measurements.append(measurement)
-                measurements.append(-measurement)
+                    image = cv2.imread(current_path)
 
-    return lines, np.array(images), np.array(measurements)
+                    images.append(image)
+                    images.append(np.fliplr(image))
+
+                    measurement = float(batch_sample[3])
+                    if i == 1:
+                        angles.append(measurement+0.2)
+                        angles.append(-measurement-0.2)
+                    elif i == 2:
+                        angles.append(measurement-0.2)
+                        angles.append(-measurement+0.2)
+                    else:
+                        angles.append(measurement)
+                        angles.append(-measurement)
+
+            # trim image to only see section with road
+            X_train = np.array(images)
+            y_train = np.array(angles)
+            yield sklearn.utils.shuffle(X_train, y_train)
 
 
 def create_model():
@@ -62,18 +79,22 @@ def create_model():
     model.compile(optimizer='adam', loss='mse')
     return model
 
-def train_model(model, X_train, y_train):
-    
-    model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=20)
+def train_model(model, samples):
+
+    train_samples, validation_samples = train_test_split(samples,test_size=0.15)
+
+    train_generator = generator(train_samples, batch_size=32)
+    validation_generator = generator(validation_samples, batch_size=32)
+
+    model.fit_generator(train_generator, samples_per_epoch= len(train_samples), validation_data=validation_generator,   nb_val_samples=len(validation_samples), nb_epoch=5, verbose=1)
 
     # Save model
     model.save('model.h5')
 
-if __name__ == "__main__":
-    print(device_lib.list_local_devices())
-    K.tensorflow_backend._get_available_gpus()
+    model.summary()
 
-    lines, X_train, y_train = read_data("./data/")
+if __name__ == "__main__":
+    samples = read_lines()
     model = create_model()
 
-    train_model(model, X_train, y_train)
+    train_model(model, samples)
